f752493448a0f25ae8c442011acd9f260227dca0
Samuel Donkor <calvinsamueldonkor@gmail.com>
2024-07-24 10:04:44 +0000

    cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,19 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    // ...
+    upload: {
+      config: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: env('signtalkval'),
+          api_key: env('368596498861456'),
+          api_secret: env('dmo7rKJEBuY6cWfSBEcfuGaqIN4'),
+        },
+        actionOptions: {
+          upload: {},
+          delete: {},
+        },
+      },
+    },
+    // ...
+  });
+  
