606057070befe781747024c2205e3354da499708
signtalk70 <signtalk70@gmail.com>
2024-08-23 08:35:12 +0000

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,7 +1,10 @@
 module.exports = ({ env }) => ({
-  proxy: true,
-  url: env('HOST_URL'), // Sets the public URL of the application.
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS')
+    keys: env.array('APP_KEYS'),
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
